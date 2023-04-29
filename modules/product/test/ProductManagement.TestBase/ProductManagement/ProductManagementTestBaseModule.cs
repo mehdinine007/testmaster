82e6577874585fbf851dd784ac25fd1f9e6cbfa6
dayani <dayani@iranfava.com>
2023-04-29 12:30:25 +0330

    add microservice project

---
+++ modules/product/test/ProductManagement.TestBase/ProductManagement/ProductManagementTestBaseModule.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.Authorization;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+using Volo.Abp.Uow;
+
+namespace ProductManagement
+{
+    [DependsOn(
+        typeof(AbpAutofacModule),
+        typeof(AbpTestBaseModule),
+        typeof(AbpAuthorizationModule),
+        typeof(ProductManagementDomainModule)
+        )]
+    public class ProductManagementTestBaseModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAlwaysAllowAuthorization();
+
+            Configure<AbpUnitOfWorkDefaultOptions>(options =>
+            {
+                options.TransactionBehavior = UnitOfWorkTransactionBehavior.Disabled;
+            });
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            SeedTestData(context);
+        }
+
+        private static void SeedTestData(ApplicationInitializationContext context)
+        {
+            using (var scope = context.ServiceProvider.CreateScope())
+            {
+                scope.ServiceProvider
+                    .GetRequiredService<ProductManagementTestDataBuilder>()
+                    .Build();
+            }
+        }
+    }
+}
