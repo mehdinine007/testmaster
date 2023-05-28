95a4f086fd30ef0d7bb29cfc6d3b68aa94e70c62
FAVA\ehsani <ehsani@iranfava.com>
2023-05-28 10:38:35 +0330

    add MellatCallBackUrl to appsetting add Mellat service implement mellat psp completely change some method from async to sync add FilterParams to payment add mellat and irankish docs

---
+++ modules/payment/PaymentManagement.Application/PaymentManagement/Mellat/Encrypt.cs
@@ -0,0 +1,33 @@
+﻿using NPOI.POIFS.Crypt;
+using System.Text;
+
+namespace PaymentManagement.Application.Mellat
+{
+    public static class Encrypt
+    {
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public static string EncryptPinBlock(byte[] key, byte[] pinBlock) //throws Exception
+        {
+            Cipher cipher = Cipher.GetInstance("DES/ECB/PKCS5Padding");
+            SecretKeySpec keySpec = new SecretKeySpec(key, "DES");
+            cipher.Init(Cipher.ENCRYPT_MODE, keySpec);
+            byte[] enc = cipher.DoFinal(pinBlock);
+            return ByteArrayToString(enc);
+        }
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+    }
+}
---
+++ modules/payment/PaymentManagement.EntityFrameworkCore/Migrations/20230528070709_AddFilterParam4.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentManagement.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFilterParam4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FilterParam4",
+                schema: "dbo",
+                table: "Payment",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilterParam4",
+                schema: "dbo",
+                table: "Payment");
+        }
+    }
+}
