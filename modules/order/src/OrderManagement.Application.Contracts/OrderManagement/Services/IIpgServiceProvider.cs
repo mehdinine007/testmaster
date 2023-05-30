e9eb85c00a8eb102b283c54c29efade909125ea5
hosseinzadeh <hosseinzadeh@HOSSEINZADEH-PC>
2023-05-30 17:47:21 +0330

    M

--- modules/order/src/OrderManagement.Application.Contracts/OrderManagement/Services/IIpgServiceProvider.cs
+++ modules/order/src/OrderManagement.Application.Contracts/OrderManagement/Services/IIpgServiceProvider.cs
@@ -13,6 +13,6 @@ namespace OrderManagement.Application.Contracts.Services
         Task<ApiResult<PspInteractionResult>> VerifyTransaction(int paymentId);
 
         Task ReverseTransaction(int paymentId);
-        Task<PspInteractionResult> VerifyTransaction(int paymentId);
+        Task RetryForVerify();
     }
 }
