e6d3737b5af3d1673109c304915ee7010866cc90
FAVA\ehsani <ehsani@iranfava.com>
2023-06-06 15:30:59 +0330

    add form Id

--- modules/payment/PaymentManagement.Application/PaymentManagement/Utilities/StringUtil.cs
+++ modules/payment/PaymentManagement.Application/PaymentManagement/Utilities/StringUtil.cs
@@ -8,7 +8,7 @@ namespace PaymentManagement.Application.Utilities
         {
             var result = string.Empty;
 
-            result += string.Format("<form name='form1' method='{0}' action='{1}' >", methodName, url);
+            result += string.Format("<form name='form1' id='form1' method='{0}' action='{1}' >", methodName, url);
             for (int i = 0; i < inputs.Keys.Count; i++)
             {
                 result += string.Format("<input name='{0}' type='hidden' value='{1}'>", inputs.Keys[i], inputs[inputs.Keys[i]]);
