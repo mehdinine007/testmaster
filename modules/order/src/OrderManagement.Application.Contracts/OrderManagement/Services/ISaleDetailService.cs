efa63267a17b5a4346523bd3008cb3593011f319
hashemi <hashemi@outlook.com>
2023-06-19 13:04:11 +0330

    crud saledetail

---
+++ modules/order/src/OrderManagement.Application.Contracts/OrderManagement/Services/ISaleDetailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace OrderManagement.Application.Contracts.OrderManagement.Services
+{
+    public interface ISaleDetailService:IApplicationService
+    {
+        Task<List<SaleDetailDto>> GetSaleDetails();
+        Task<int> Save(SaleDetailDto saleDetail);
+        Task<int> Update(SaleDetailDto saleDetail);
+        Task<int> Delete(int id);
+    }
+}
---
+++ modules/order/src/OrderManagement.Application/OrderManagement/Implementations/SaleDetailService.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper.Internal.Mappers;
+using OrderManagement.Application.Contracts;
+using OrderManagement.Application.Contracts.OrderManagement.Services;
+using OrderManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.ObjectMapping;
+using Volo.Abp.Uow;
+
+namespace OrderManagement.Application.OrderManagement.Implementations;
+
+public class SaleDetailService : ApplicationService, ISaleDetailService
+{
+    private readonly IRepository<SaleDetail> _saleDetailRepository;
+
+    public SaleDetailService(IRepository<SaleDetail> saleDetailRepository)
+    {
+        _saleDetailRepository = saleDetailRepository;
+    }
+
+
+
+    public async Task<int> Delete(int id)
+    {
+
+        await _saleDetailRepository.DeleteAsync(x => x.Id == id);
+        await CurrentUnitOfWork.SaveChangesAsync();
+        return id;
+    }
+
+    public async Task<List<SaleDetailDto>> GetSaleDetails()
+    {
+        var saleDetails = await _saleDetailRepository.WithDetailsAsync(x => x.CarTip, x => x.CarTip.CarType);
+        var queryResult = saleDetails.ToList();
+        var saleDetailDtos = ObjectMapper.Map<List<SaleDetail>, List<SaleDetailDto>>(queryResult);
+        return saleDetailDtos;
+
+    }
+    [UnitOfWork(isTransactional: false)]
+    public async Task<int> Save(SaleDetailDto saleDetailDto)
+    {
+        SaleDetail saleDetail = new SaleDetail()
+        {
+
+            CarTipId = saleDetailDto.CarTipId,
+            ESaleTypeId = saleDetailDto.EsaleTypeId,
+            CarFee = saleDetailDto.CarFee,
+            CircularSaleCode = saleDetailDto.CircularSaleCode,
+            CarDeliverDate = saleDetailDto.CarDeliverDate,
+            SalePlanCode = saleDetailDto.SalePlanCode,
+            SaleTypeCapacity = saleDetailDto.SaleTypeCapacity,
+            SalePlanDescription = saleDetailDto.SalePlanDescription,
+            DeliverDaysCount= saleDetailDto.DeliverDaysCount,
+            CoOperatingProfitPercentage= saleDetailDto.CoOperatingProfitPercentage,
+            MinimumAmountOfProxyDeposit = saleDetailDto.MinimumAmountOfProxyDeposit,
+             
+        };
+
+        //var saleDetail = ObjectMapper.Map<SaleDetailDto, SaleDetail> (saleDetailDto);
+        await _saleDetailRepository.InsertAsync(saleDetail);
+        await CurrentUnitOfWork.SaveChangesAsync();
+        return saleDetail.Id;
+    }
+
+    public async Task<int> Update(SaleDetailDto saleDetailDto)
+    {
+        //var saleDetail = ObjectMapper.Map<SaleDetailDto, SaleDetail>(saleDetailDto);
+        SaleDetail saleDetail = new SaleDetail()
+        {
+           
+            CarTipId = saleDetailDto.CarTipId,
+            ESaleTypeId = saleDetailDto.EsaleTypeId,
+            CarFee = saleDetailDto.CarFee,
+            CircularSaleCode = saleDetailDto.CircularSaleCode,
+            CarDeliverDate = saleDetailDto.CarDeliverDate,
+            SalePlanCode = saleDetailDto.SalePlanCode,
+            SaleTypeCapacity = saleDetailDto.SaleTypeCapacity,
+            SalePlanDescription = saleDetailDto.SalePlanDescription,
+            DeliverDaysCount = saleDetailDto.DeliverDaysCount,
+            CoOperatingProfitPercentage = saleDetailDto.CoOperatingProfitPercentage,
+            MinimumAmountOfProxyDeposit = saleDetailDto.MinimumAmountOfProxyDeposit,
+
+        };
+     
+        await _saleDetailRepository.UpdateAsync(saleDetail);
+        await CurrentUnitOfWork.SaveChangesAsync();
+        return saleDetail.Id;
+    }
+}
+
