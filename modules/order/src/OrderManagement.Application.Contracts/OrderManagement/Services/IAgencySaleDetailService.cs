aee70a4d309f86e6205af389449aa3a0c97a94e7
hashemi <hashemi@outlook.com>
2023-06-20 10:59:08 +0330

    crud AgencySaleDetail

---
+++ modules/order/src/OrderManagement.Application.Contracts/OrderManagement/Services/IAgencySaleDetailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace OrderManagement.Application.Contracts.OrderManagement.Services
+{
+    public interface IAgencySaleDetailService: IApplicationService
+    {
+        Task<List<AgencySaleDetailListDto>> GetAgencySaleDetail(int saleDetailId);
+        Task<int> Save(AgencySaleDetailDto agencySaleDetailDto);
+        Task<int> Delete(int id);
+
+    }
+}
---
+++ modules/order/src/OrderManagement.Application/OrderManagement/Implementations/AgencySaleDetailService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper.Internal.Mappers;
+using OrderManagement.Application.Contracts;
+using OrderManagement.Application.Contracts.OrderManagement;
+using OrderManagement.Application.Contracts.OrderManagement.Services;
+using OrderManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Uow;
+
+namespace OrderManagement.Application.OrderManagement.Implementations
+{
+    public class AgencySaleDetailService : ApplicationService, IAgencySaleDetailService
+    {
+
+        private readonly IRepository<AgencySaleDetail> _agencySaleDetailRepository;
+
+        public AgencySaleDetailService(IRepository<AgencySaleDetail> agencySaleDetailRepository)
+        {
+            _agencySaleDetailRepository = agencySaleDetailRepository;
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            await _agencySaleDetailRepository.DeleteAsync(x => x.Id == id);
+            await CurrentUnitOfWork.SaveChangesAsync();
+            return id;
+        }
+
+        public async Task<List<AgencySaleDetailListDto>> GetAgencySaleDetail(int saleDetailId)
+        {
+            var agencySaleDetail = await _agencySaleDetailRepository.WithDetailsAsync(x => x.Agency, x => x.SaleDetail);
+            var queryResult = agencySaleDetail.Where(x => x.SaleDetailId == saleDetailId).ToList();
+            var agencySaleDetailDto = ObjectMapper.Map<List<AgencySaleDetail>, List<AgencySaleDetailListDto>>(queryResult);
+            return agencySaleDetailDto;
+
+        }
+        [UnitOfWork]
+        public async Task<int> Save(AgencySaleDetailDto agencySaleDetailDto)
+        {
+            var agencySaleDetail = ObjectMapper.Map<AgencySaleDetailDto, AgencySaleDetail>(agencySaleDetailDto);
+           
+            await _agencySaleDetailRepository.InsertAsync(agencySaleDetail);
+            await CurrentUnitOfWork.SaveChangesAsync();
+            return agencySaleDetail.Id;
+        }
+    }
+}
