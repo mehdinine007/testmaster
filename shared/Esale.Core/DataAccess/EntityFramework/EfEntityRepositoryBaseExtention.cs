bc3b8ff84b27c87f580c084a5301ebfafb23364f
Nohtorki <Nohtorki@NOHTORKI-PC>
2023-06-24 07:58:21 +0330

    اضافه شدن متد RemoveTracking

--- shared/Esale.Core/DataAccess/EntityFramework/EfEntityRepositoryBaseExtention.cs
+++ shared/Esale.Core/DataAccess/EntityFramework/EfEntityRepositoryBaseExtention.cs
@@ -9,7 +9,7 @@ using Volo.Abp.EntityFrameworkCore;
 namespace Esale.Core.DataAccess
 {
     public static class EfEntityRepositoryBaseExtention
-    { 
+    {
         public static async Task<TEntity> AttachAsync<TEntity>(this IReadOnlyBasicRepository<TEntity> repository, TEntity entity, params Expression<Func<TEntity, object>>[] properties)
         where TEntity : class, IEntity
         {
@@ -33,11 +33,25 @@ namespace Esale.Core.DataAccess
             {
                 dbContext.Entry(entity).State = EntityState.Modified;
             }
-            
+
             dbContext.SaveChanges();
             dbContext.Entry(entity).State = EntityState.Detached;
             return entity;
         }
-       
+        public static async Task GetTracker<TEntity>(this IReadOnlyBasicRepository<TEntity> repository, TEntity entity)
+         where TEntity : class, IEntity
+        {
+            var dbContext = repository.ToEfCoreRepository().DbContext;// await GetDbContextAsync();
+            var x = dbContext.ChangeTracker.Entries<TEntity>().ToList();
+
+        }
+        public static async Task RemoveTracking<TEntity>(this IReadOnlyBasicRepository<TEntity> repository, TEntity entity)
+       where TEntity : class, IEntity
+        {
+            var dbContext = repository.ToEfCoreRepository().DbContext;// await GetDbContextAsync();
+            dbContext.Entry(entity).State = EntityState.Detached;
+
+
+        }
     }
 }
