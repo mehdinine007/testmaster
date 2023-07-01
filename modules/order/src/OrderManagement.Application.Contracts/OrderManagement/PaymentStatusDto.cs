efa3ff438c61d79722a8b27907646b198f75f335
dayani <dayani@iranfava.com>
2023-07-01 09:37:34 +0330

    nullable type property in proto

--- modules/order/src/OrderManagement.Application.Contracts/OrderManagement/PaymentStatusDto.cs
+++ modules/order/src/OrderManagement.Application.Contracts/OrderManagement/PaymentStatusDto.cs
@@ -5,9 +5,9 @@ namespace OrderManagement.Application.Contracts
     public class PaymentStatusDto
     {
         public int RelationId { get; set; }
-        public int RelationIdB { get; set; }
-        public int RelationIdC { get; set; }
-        public int RelationIdD { get; set; }
+        public int? RelationIdB { get; set; }
+        public int? RelationIdC { get; set; }
+        public int? RelationIdD { get; set; }
         public bool IsRelationIdGroup  { get; set; }
         public bool IsRelationIdBGroup { get; set; }
         public bool IsRelationIdCGroup { get; set; }
