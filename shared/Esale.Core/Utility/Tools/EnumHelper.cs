7d59226c382388566d66795b2c88e5f7f8cf0da3
dayani <dayani@iranfava.com>
2023-07-24 14:47:24 +0330

    add attachmentviewmodel

---
+++ shared/Esale.Core/Utility/Tools/EnumHelper.cs
@@ -0,0 +1,37 @@
+﻿using Esale.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Esale.Core.Utility.Tools
+{
+    public static class EnumHelper
+    {
+        public static EnumValueList GetField(Enum _enum, int enumValue)
+        {
+            EnumPropertyAttribute attribute = _enum.GetType()
+                .GetField(_enum.ToString())
+                .GetCustomAttributes(typeof(EnumPropertyAttribute), false)
+                .SingleOrDefault() as EnumPropertyAttribute;
+            return attribute == null
+                ? new EnumValueList()
+                : new EnumValueList()
+                {
+                    Key = _enum.ToString(),
+                    Value = enumValue,
+                    DisplayTitle = attribute.Description
+                };
+        }
+
+        public static string GetDescription(Enum _enum)
+        {
+            EnumPropertyAttribute attribute = _enum.GetType()
+                .GetField(_enum.ToString())
+                .GetCustomAttributes(typeof(EnumPropertyAttribute), false)
+                .SingleOrDefault() as EnumPropertyAttribute;
+            return attribute == null ? "" : attribute.Description;
+        }
+
+    }
+}
---
+++ shared/Esale.Core/Utility/Tools/StringHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Utility.Tools
+{
+    public static class StringHelper
+    {
+        public static string Repeat (string _str,int length)
+        {
+            return string.Concat(Enumerable.Repeat("0", length - _str.Length)) + _str;
+        }
+    }
+}
