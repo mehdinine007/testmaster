c3c383de84ce250a41bb190f53ffac785f4dead1
hashemi <hashemi@outlook.com>
2023-07-26 11:05:05 +0330

    remove cartip,cartype,company,carfamily from saledto

---
+++ modules/order/src/OrderManagement.Application.Contracts/OrderManagement/Models/ProductAndCategoryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using OrderManagement.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.Contracts.OrderManagement.Models
+{
+    public class ProductAndCategoryViewModel
+    {
+
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Title { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public int LevelId { get; set; }
+
+        public List<AttachmentViewModel> Attachments { get; set; }
+
+
+    }
+}
