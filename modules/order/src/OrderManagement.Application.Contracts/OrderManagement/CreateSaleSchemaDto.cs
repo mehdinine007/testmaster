54b048479a82900cb7d69340e0ec7d4c373d21d4
hashemi <hashemi@outlook.com>
2023-08-09 10:21:28 +0330

    add CreateSaleSchemaDto

---
+++ modules/order/src/OrderManagement.Application.Contracts/OrderManagement/CreateSaleSchemaDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.Contracts.OrderManagement
+{
+    public class CreateSaleSchemaDto
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int SaleStatus { get; set; }
+    }
+}
