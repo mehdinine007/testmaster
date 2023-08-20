fc525fa22470bdb4672a4813320c16c73d4d9e8d
dayani <dayani@iranfava.com>
2023-08-20 11:22:02 +0330

    run hangfire

---
+++ microservices/OrderService.Host/Infrastructures/Hangfire/DashboardNoAuthorizationFilter.cs
@@ -0,0 +1,12 @@
+﻿using Hangfire.Dashboard;
+
+namespace OrderService.Host.Infrastructures.Hangfire
+{
+    public class DashboardNoAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext dashboardContext)
+        {
+            return true;
+        }
+    }
+}
