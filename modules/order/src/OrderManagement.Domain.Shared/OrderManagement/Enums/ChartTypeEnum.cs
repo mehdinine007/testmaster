3949b4c43b7359fee2cd355d6c0f70848a08f8a4
dayani <dayani@iranfava.com>
2023-08-21 09:45:44 +0330

    ISM-3634 : get list chartstructure

--- modules/order/src/OrderManagement.Domain.Shared/OrderManagement/Enums/ChartTypeEnum.cs
+++ modules/order/src/OrderManagement.Domain.Shared/OrderManagement/Enums/ChartTypeEnum.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Esale.Core.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,13 @@ namespace OrderManagement.Domain.Shared
 {
     public enum ChartTypeEnum
     {
+        [EnumProperty(Description = "میله ای")]
         Bar = 1,
+        [EnumProperty(Description = "ستونی")]
         Column = 2,
+        [EnumProperty(Description = "دایره ای")]
         Pie = 3,
+        [EnumProperty(Description = "خطی")]
         Line =4
     }
 }
