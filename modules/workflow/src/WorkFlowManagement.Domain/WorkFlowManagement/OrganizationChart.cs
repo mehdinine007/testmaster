7e676b359b5c684d0f27288351d9212e6c739e48
dayani <dayani@iranfava.com>
2023-08-28 10:49:49 +0330

    feat(ISM-3764):add hasset in chart entity

--- modules/workflow/src/WorkFlowManagement.Domain/WorkFlowManagement/OrganizationChart.cs
+++ modules/workflow/src/WorkFlowManagement.Domain/WorkFlowManagement/OrganizationChart.cs
@@ -9,12 +9,16 @@ namespace WorkFlowManagement.Domain.WorkFlowManagement
 {
     public class OrganizationChart : FullAuditedEntity<int>
     {
-        public string? Code { get; set; }
-        public string? Title { get; set; }
+        public OrganizationChart()
+        {
+            Childrens = new HashSet<OrganizationChart>();
+        }
+        public string Code { get; set; }
+        public string Title { get; set; }
         public bool Status { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
         public int? ParentId { get; set; }
         public virtual OrganizationChart Parent { get; set; }
-        public virtual ICollection<OrganizationChart> Childrens { get;set; }
-        }
-        }
+        public virtual ICollection<OrganizationChart> Childrens { get; set; }
+    }
+}
