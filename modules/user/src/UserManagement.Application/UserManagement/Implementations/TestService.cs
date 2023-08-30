5393d6c05d2f5349b2205938f0b3f36396989bc1
erfankaviannew <erfan>
2023-08-30 12:49:54 +0330

    feat(ISM-3689):user module host test

---
+++ modules/user/src/UserManagement.Application/UserManagement/Implementations/TestService.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace UserManagement.Application.UserManagement.Implementations
+{
+    public static class TestService
+    {
+        public static List<string> GetTestData()
+        {
+            return new List<string> 
+            { "Erfan Test data 1", 
+              "Erfan Test data 2", 
+              "Erfan Test data 3" };
+        }
+    }
+}
---
+++ modules/user/src/UserManagement.HttpApi/UserManagement/Controller/TestController.cs
@@ -0,0 +1,20 @@
+﻿#region NS
+using Microsoft.AspNetCore.Mvc;
+using UserManagement.Application.UserManagement.Implementations;
+using Volo.Abp.AspNetCore.Mvc;
+# endregion
+
+
+namespace UserManagement.HttpApi.UserManagement.Controller
+{
+    [Route("api/Test")]
+    public class TestController : AbpController
+    {
+        [HttpGet("TestData")]
+        public IActionResult GetTestData()
+        {
+            var testData = TestService.GetTestData();
+            return new ObjectResult(testData);
+        }
+    }
+}
