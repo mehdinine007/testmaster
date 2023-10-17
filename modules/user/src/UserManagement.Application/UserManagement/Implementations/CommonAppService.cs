695dd7553cc22fe63e705a5391d47bd818dd56f1
zolfaghari <you@example.com>
2023-10-17 10:57:00 +0330

    fix(ISM-4567):bug fix ValidateSMS

--- modules/user/src/UserManagement.Application/UserManagement/Implementations/CommonAppService.cs
+++ modules/user/src/UserManagement.Application/UserManagement/Implementations/CommonAppService.cs
@@ -131,9 +131,7 @@ public class CommonAppService : ApplicationService, ICommonAppService
 
         //string ObjectSMSCode = RedisHelper.GetDatabase().StringGet(sMSType.ToString() + Mobile + NationalCode);
         //var ObjectSMSCode = await _distributedCache.GetStringAsync(sMSType.ToString() + Mobile + NationalCode);
-        var PreFix = "";
-        var ObjectSMSCode = await _cacheManager.GetStringAsync(Mobile + NationalCode, PreFix, new() { Provider = CacheProviderEnum.Redis });
-
+        var ObjectSMSCode = await _cacheManager.GetStringAsync(Mobile + NationalCode, sMSType.ToString(), new() { Provider = CacheProviderEnum.Redis });
 
 
         if (ObjectSMSCode == null)
