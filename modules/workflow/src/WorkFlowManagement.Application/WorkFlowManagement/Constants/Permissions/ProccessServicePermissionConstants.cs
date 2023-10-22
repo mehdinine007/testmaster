e034b241ccff3a726dcb7afba7318bce5b127710
haghighat <sh.f.haghighat@gmail.com>
2023-10-22 13:54:14 +0330

    feat(ISM-4601) : Add DisplayName to constants

--- modules/workflow/src/WorkFlowManagement.Application/WorkFlowManagement/Constants/Permissions/ProccessServicePermissionConstants.cs
+++ modules/workflow/src/WorkFlowManagement.Application/WorkFlowManagement/Constants/Permissions/ProccessServicePermissionConstants.cs
@@ -8,8 +8,7 @@ public class ProccessServicePermissionConstants : BasePermissionConstants
     public const string StartProcess_DisplayName = "شروع پردازش";
     public const string GetOutBox = "000300020002";
     public const string GetOutBox_DisplayName = "نمایش کارتابل ارسال شده";
-    public const string GetOutBox_Title = "000300020002";
-    public const string GetOutBox_Title_DisplayName = "نمایش عنوان کارتابل ارسال شده";
+
 
     public override string ModuleIdentifier => "0003";
     public override string ServiceIdentifier => "0002";
