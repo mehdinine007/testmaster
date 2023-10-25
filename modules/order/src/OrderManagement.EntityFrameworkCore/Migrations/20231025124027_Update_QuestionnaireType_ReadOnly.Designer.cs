3b3ad2f2272c201f6df6e402ea6ad70b55369240
hosseinzadeh <hosseinzadeh@HOSSEINZADEH-PC>
2023-10-25 16:12:10 +0330

    feat(ISM-4648) : update questionnaire type

--- modules/order/src/OrderManagement.EntityFrameworkCore/Migrations/20231025124027_Update_QuestionnaireType_ReadOnly.Designer.cs
+++ modules/order/src/OrderManagement.EntityFrameworkCore/Migrations/20231025124027_Update_QuestionnaireType_ReadOnly.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrderManagement.EfCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace OrderManagement.EfCore.Migrations
 {
     [DbContext(typeof(OrderManagementDbContext))]
-    partial class OrderManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231025124027_Update_QuestionnaireType_ReadOnly")]
+    partial class UpdateQuestionnaireTypeReadOnly
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
