de68b8a0a14c545281fb55e4638f539636df0a9c
dayani <dayani@iranfava.com>
2023-11-04 13:00:30 +0330

    feat(ISM-4754):add licence and version in all module

---
+++ shared/Licence/LicenceInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Licence
+{
+    public class LicenceInfo
+    {
+        public bool IsValid { get; set; }
+        public string SerialNumber { get; set; }
+
+        public string PublicKey { get; set; }
+
+        public List<string> Modules { get; set; }
+
+        public string OrganizationTitle { get; set; }
+
+    }
+}
