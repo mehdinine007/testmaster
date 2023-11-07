00063b80593812b0d4e49b2247b70be7f9d642ab
dayani <dayani@iranfava.com>
2023-11-07 08:32:42 +0330

    fix:add version 4.0.1-rc.1

--- shared/Licence/AppLicence.cs
+++ shared/Licence/AppLicence.cs
@@ -2,7 +2,7 @@
 {
     public class AppLicence
     {
-        public static string Version = "4.0.0";
+        public static string Version = "4.0.1-rc.1";
         public static string ReleaseDate  = "1402/08/14";
         public static LicenceInfo GetLicence(string serialNumber)
         {
