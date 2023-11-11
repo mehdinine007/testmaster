cbfe4fbb31c2323e4c3749086de8e9d97ac01e4f
hashemi <hashemi@outlook.com>
2023-11-11 17:49:03 +0330

    feat(ISM-4820):add authenticate for admin panel

---
+++ modules/adminPanel/src/AdminPanelManagement.Application.Contracts/AdminPanelManagement/IServices/IAuthenticateAppService.cs
@@ -0,0 +1,16 @@
+﻿using AdminPanelManagement.Application.Contracts.AdminPanelManagement.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace AdminPanelManagement.Application.Contracts.AdminPanelManagement.IServices
+{
+    public interface IAuthenticateAppService: IApplicationService
+    {
+
+        Task<AuthenticateResultModel> Authenticate(AuthenticateReqDto model);
+    }
+}
---
+++ modules/adminPanel/src/AdminPanelManagement.Application.Contracts/AdminPanelManagement/IServices/IUserGrpcClientService.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanelManagement.Application.Contracts.AdminPanelManagement.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanelManagement.Application.Contracts.AdminPanelManagement.IServices
+{
+    public interface IUserGrpcClientService
+    {
+        Task<AuthenticateResponseDto> Athenticate(AuthenticateReqDto input);
+    }
+}
