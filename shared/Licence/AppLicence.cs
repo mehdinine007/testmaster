71eb3958e01febd609209f0c5d7bc7756a95a690
Nohtorki <Nohtorki@NOHTORKI-PC>
2023-11-13 10:07:49 +0330

    fix:update license

--- shared/Licence/AppLicence.cs
+++ shared/Licence/AppLicence.cs
@@ -2,7 +2,7 @@
 {
     public class AppLicence
     {
-        public static string Version = "4.0.2-rc.1";
+        public static string Version = "4.0.3-rc.1";
         public static string ReleaseDate  = "1402/08/17";
         public static LicenceInfo GetLicence(string serialNumber)
         {
