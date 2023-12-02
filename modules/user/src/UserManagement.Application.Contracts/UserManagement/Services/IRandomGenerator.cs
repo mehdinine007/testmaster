e5d7a1a1314c4a72b8504de6260e38bb8c9ecac0
haghighat <sh.f.haghighat@gmail.com>
2023-12-02 12:11:10 +0330

    feat(ISM-5116) : add GetUniqueInt to share for smscode and trackingcode

--- modules/user/src/UserManagement.Application.Contracts/UserManagement/Services/IRandomGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Application.Services;
-
-namespace UserManagement.Application.Contracts.UserManagement.Services
-{
-    public interface IRandomGenerator : IApplicationService
-    {
-        int GetUniqueInt();
-    }
-}
