337004d8a54f737e8429636df754aa3a3d0df8d0
dayani <dayani@iranfava.com>
2024-01-13 15:16:37 +0330

    fix:add datafirst migration in company module

---
+++ microservices/CompanyService.Host/Controllers/MigrationController.cs
@@ -0,0 +1,25 @@
+﻿using IFG.Core.Utility.Migration;
+using Licence;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+
+namespace CompanyService.Controllers
+{
+    [RemoteService]
+    [Route("api/services/app/[controller]/[action]")]
+    public class MigrationController : Controller
+    {
+        private readonly MigrationsHistoryService _migrationsHistoryService;
+        public MigrationController()
+        {
+            _migrationsHistoryService = new MigrationsHistoryService("CompanyManagement", AppLicence.GetVersion("").FixVersion);
+        }
+
+        [HttpPost]
+        public bool UpdateDataBase()
+        {
+            return _migrationsHistoryService.UpdateDatabase();
+        }
+
+    }
+}
