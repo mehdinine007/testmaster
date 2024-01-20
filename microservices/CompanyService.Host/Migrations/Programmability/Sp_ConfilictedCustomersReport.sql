5d9f4377d593e5844ba551a98d59c4f88c78a5e9
dayani <dayani@iranfava.com>
2024-01-20 15:39:16 +0330

    fix(CSDD-300):add confilicted customers report

---
+++ microservices/CompanyService.Host/Migrations/Programmability/Sp_ConfilictedCustomersReport.sql
@@ -0,0 +1,13 @@
+CREATE proc Sp_ConfilictedCustomersReport
+	as
+begin
+  	select [کد ملی]=nationalcode 
+	      ,[تعداد خرید]=count(0) 
+	from ConfilictedCustomers 
+	where variz >= 1
+	group by nationalcode
+	having count(0) > 1
+end
+
+
+
