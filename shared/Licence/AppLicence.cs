2edbb1445a540f1d20ea7c35cb2b7c361de223f0
dayani <dayani@iranfava.com>
2024-01-24 08:44:08 +0330

    fix:add version 4.2.0 final

--- shared/Licence/AppLicence.cs
+++ shared/Licence/AppLicence.cs
@@ -2,9 +2,9 @@
 {
     public class AppLicence
     {
-        public static string Version = "4.2.0-rc.5";
+        public static string Version = "4.2.0";
         public static string FixVersion = "4.2.0";
-        public static string ReleaseDate = "1402/11/03";
+        public static string ReleaseDate = "1402/11/04";
         public static LicenceInfo GetLicence(string serialNumber)
         {
             LicenceInfo licenceInfo = new LicenceInfo();
