628eb124aa16911835bb697b192f9e3fa977ec2f
hashemi <hashemi@HASHEMI-NB>
2024-04-08 09:22:44 +0330

    fix(CSDD-443):bug fix commitorder

--- modules/order/src/OrderManagement.Migrator/Migrations/Patch/20240407120546_remove-CircularSaleCode_SaleStatus.sql
+++ modules/order/src/OrderManagement.Migrator/Migrations/Patch/20240407120546_remove-CircularSaleCode_SaleStatus.sql
@@ -5,10 +5,10 @@ FROM [sys].[default_constraints] [d]
 INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
 WHERE ([d].[parent_object_id] = OBJECT_ID(N'[SaleDetail]') AND [c].[name] = N'CircularSaleCode');
 IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [SaleDetail] DROP CONSTRAINT [' + @var0 + '];');
-ALTER TABLE [SaleDetail] DROP COLUMN [CircularSaleCode];
+exec(N'ALTER TABLE [SaleDetail] DROP COLUMN [CircularSaleCode]');
 end
 if  exists(select 1 from INFORMATION_SCHEMA.COLUMNS where Upper(TABLE_NAME)= 'SaleSchema' and Upper(COLUMN_NAME)='SaleStatus' ) begin
-     ALTER TABLE SaleSchema DROP COLUMN SaleStatus;
+     exec(N'ALTER TABLE SaleSchema DROP COLUMN SaleStatus');
 end
 
 exec(N'ALTER TABLE SaleSchema
