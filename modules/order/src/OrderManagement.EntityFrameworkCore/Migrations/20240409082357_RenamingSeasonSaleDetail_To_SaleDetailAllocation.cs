0a053ebc9e4379f62a071a9889edd66bd8468935
hosseinzadeh <hosseinzadeh@HOSSEINZADEH-PC>
2024-04-09 13:31:40 +0330

    feat(CSDD-481): rename table SeasonCompanyProduct to SaleDetailAllocation

--- modules/order/src/OrderManagement.EntityFrameworkCore/Migrations/20240409082357_RenamingSeasonSaleDetail_To_SaleDetailAllocation.cs
+++ modules/order/src/OrderManagement.EntityFrameworkCore/Migrations/20240409082357_RenamingSeasonSaleDetail_To_SaleDetailAllocation.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -11,107 +10,13 @@ namespace OrderManagement.EfCore.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "SeasonCompanyProduct");
-
-            migrationBuilder.CreateTable(
-                name: "SaleDetailAllocation",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    SeasonId = table.Column<int>(type: "int", nullable: false),
-                    SaleDetailId = table.Column<int>(type: "int", nullable: false),
-                    Count = table.Column<int>(type: "int", nullable: false),
-                    IsComplete = table.Column<bool>(type: "bit", nullable: false),
-                    YearId = table.Column<int>(type: "int", nullable: true),
-                    TotalCount = table.Column<int>(type: "int", nullable: true),
-                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
-                    DeleterId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_SaleDetailAllocation", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_SaleDetailAllocation_SaleDetail_SaleDetailId",
-                        column: x => x.SaleDetailId,
-                        principalTable: "SaleDetail",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_SaleDetailAllocation_Year_YearId",
-                        column: x => x.YearId,
-                        principalTable: "Year",
-                        principalColumn: "Id");
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_SaleDetailAllocation_SaleDetailId",
-                table: "SaleDetailAllocation",
-                column: "SaleDetailId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_SaleDetailAllocation_YearId",
-                table: "SaleDetailAllocation",
-                column: "YearId");
+            migrationBuilder.RenameTable("SeasonCompanyProduct", null, "SaleDetailAllocation");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "SaleDetailAllocation");
-
-            migrationBuilder.CreateTable(
-                name: "SeasonCompanyProduct",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    SaleDetailId = table.Column<int>(type: "int", nullable: false),
-                    YearId = table.Column<int>(type: "int", nullable: true),
-                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    DeleterId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    IsComplete = table.Column<bool>(type: "bit", nullable: false),
-                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
-                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    SeasonId = table.Column<int>(type: "int", nullable: false),
-                    TotalCount = table.Column<int>(type: "int", nullable: true),
-                    count = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_SeasonCompanyProduct", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_SeasonCompanyProduct_SaleDetail_SaleDetailId",
-                        column: x => x.SaleDetailId,
-                        principalTable: "SaleDetail",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_SeasonCompanyProduct_Year_YearId",
-                        column: x => x.YearId,
-                        principalTable: "Year",
-                        principalColumn: "Id");
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_SeasonCompanyProduct_SaleDetailId",
-                table: "SeasonCompanyProduct",
-                column: "SaleDetailId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_SeasonCompanyProduct_YearId",
-                table: "SeasonCompanyProduct",
-                column: "YearId");
+            migrationBuilder.RenameTable("SaleDetailAllocation", null, "SeasonCompanyProduct");
         }
     }
 }
