38def5850ccaf558c341f6dc285a5c2b474d60c2
dayani <dayani@iranfava.com>
2024-04-15 10:42:30 +0330

    fix:change migrator tsql for organization

--- modules/order/src/OrderManagement.Migrator/Migrations/Patch/20240221080039_update-organization.sql
+++ modules/order/src/OrderManagement.Migrator/Migrations/Patch/20240221080039_update-organization.sql
@@ -1,13 +1,20 @@
+exec(N'select * into TmpOrganization_BeforeAddCopmany from [Organization]')
 exec(N'ALTER TABLE [Organization] ALTER COLUMN [Code] nvarchar(100) NOT NULL')
 exec(N'ALTER TABLE [Organization] ADD [IsActive] bit NOT NULL DEFAULT CAST(1 AS bit)')
 exec(N'ALTER TABLE [Organization] ADD [Priority] int NOT NULL DEFAULT 0')
 exec(N'ALTER TABLE [Organization] ADD [SupportingPhone] nvarchar(50) ')
 exec(N'ALTER TABLE [Organization] ADD [UrlSite] nvarchar(300) ')
 
-exec(N'update [Organization] set Priority = convert(int,code) where Priority = 0')
+exec(N'truncate table [Organization]')
+exec(N'
+   SET IDENTITY_INSERT [dbo].[Organization] ON
+       insert into [Organization] ([Id], [Code], [Title], [CreationTime],[IsDeleted], [IsActive], [Priority])
+       select [Id], [Code], [Title], Getdate(),0, 1, convert(int,code)
+	   from ProductAndCategory
+	   where IsDeleted = 0 and ParentId is null and LevelId = 1
+   SET IDENTITY_INSERT [dbo].[Organization] OFF 
+')
 
-update [Organization]
-set [Code] = right('000' + [Code],4)
-where len(Code) < 4
+
 
 
