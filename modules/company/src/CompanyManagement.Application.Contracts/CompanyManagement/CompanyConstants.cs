ba7f48bc353a58191ba344cfdb32d37c88f1de8a
hashemi <hashemi@HASHEMI-NB>
2024-05-06 16:41:42 +0330

    feat(CSDD-560):add checkblacklistservice

---
+++ modules/company/src/CompanyManagement.Application.Contracts/CompanyManagement/CompanyConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagement.Application.Contracts.CompanyManagement
+{
+    public class CompanyConstants
+    {
+        public static string ClientsOrderDetailByCompanyNotFound = "رکوردی یافت نشد";
+        public static string ClientsOrderDetailByCompanyNotFoundId = "0001";
+
+    }
+}
---
+++ modules/company/src/CompanyManagement.Application/CompanyManagement/Implementations/BlackListService.cs
@@ -0,0 +1,41 @@
+﻿using CompanyManagement.Application.Contracts.CompanyManagement;
+using CompanyManagement.Application.Contracts.CompanyManagement.Dto;
+using CompanyManagement.Application.Contracts.CompanyManagement.Services;
+using CompanyManagement.Domain.CompanyManagement;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace CompanyManagement.Application.CompanyManagement.Implementations
+{
+    public class BlackListService : ApplicationService, IBlackListService
+    {
+
+        private readonly IRepository<ClientsOrderDetailByCompany, long> _clientsOrderDetailByCompanyRepository;
+        public BlackListService(
+            IRepository<ClientsOrderDetailByCompany, long> clientsOrderDetailByCompanyRepository)
+        {
+            _clientsOrderDetailByCompanyRepository = clientsOrderDetailByCompanyRepository;
+        }
+
+        public async Task<bool> Inquiry(string nationalCode)
+        {
+            var clientsOrderDetailByCompany = (await _clientsOrderDetailByCompanyRepository.GetQueryableAsync()).AsNoTracking()
+                .Include(x => x.Paypaidprice)
+                            .OrderByDescending(x => x.Id)
+                            .FirstOrDefault(x => x.NationalCode == nationalCode);
+            if (clientsOrderDetailByCompany is  null)
+            {
+                throw new UserFriendlyException(CompanyConstants.ClientsOrderDetailByCompanyNotFound, CompanyConstants.ClientsOrderDetailByCompanyNotFoundId);
+            }
+            return clientsOrderDetailByCompany.Paypaidprice.Any();
+           
+        }
+    }
+}
