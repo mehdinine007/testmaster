49b0d7ab4737e1aa7374becce12d782ef7ef8c68
hashemi <hashemi@HASHEMI-NB>
2024-05-12 10:47:03 +0330

    feat(CSDD-580|CSDD-581):add companyid in companyproduction

---
+++ modules/company/src/CompanyManagement.EntityFrameworkCore/Migrations/CompanyManagementDb/20240512060209_add-companyid-companyproduction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderManagement.EfCore.Migrations.CompanyManagementDb
+{
+    /// <inheritdoc />
+    public partial class addcompanyidcompanyproduction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "CompanyProduction",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "CompanyProduction");
+        }
+    }
+}
