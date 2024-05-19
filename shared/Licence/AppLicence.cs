e82214ff6589e8f6f972a3906ae8699a2a75b1a8
Nohtorki <Nohtorki@NOHTORKI-PC>
2024-05-19 12:45:01 +0330

    fix: bug userdataaccess

--- shared/Licence/AppLicence.cs
+++ shared/Licence/AppLicence.cs
@@ -2,9 +2,9 @@
 {
     public class AppLicence
     {
-        public static string Version = "4.6.0-rc.2";
+        public static string Version = "4.6.0-rc.3";
         public static string FixVersion = "4.6.0";
-        public static string ReleaseDate = "1403/02/18";
+        public static string ReleaseDate = "1403/02/30";
         public static LicenceInfo GetLicence(string serialNumber)
         {
             LicenceInfo licenceInfo = new LicenceInfo();
