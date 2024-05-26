f747cf8f329ee042aa7d603ad2efaf21f332f815
hashemi <hashemi@HASHEMI-NB>
2024-05-26 16:34:28 +0330

    fix(CSDD-592|CSDD-593):bugfix saleschema

---
+++ modules/order/src/OrderManagement.Application/OrderManagement/FluentValidation/SaleSchemaValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using OrderManagement.Application.Contracts.OrderManagement;
+using OrderManagement.Application.Contracts.OrderManagement.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.OrderManagement.FluentValidation
+{
+    public class SaleSchemaValidator: AbstractValidator<CreateSaleSchemaDto>
+    {
+        public SaleSchemaValidator()
+        {
+
+            RuleFor(x => x.Title).NotNull().NotEmpty().WithMessage(ValidationConstant.TitleNotFound);
+            RuleFor(x => x.Code).NotNull().NotEmpty().WithMessage(ValidationConstant.CodeNotFound);
+        }
+    }
+}
